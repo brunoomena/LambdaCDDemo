cd6996c902a7bf6c1afb8bc32e8d5d6a53937f50
brunoomena <omena.bru@gmail.com>
2021-03-25 11:49:30 -0300

    new try xesquedele

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -1,7 +1,35 @@
 import boto3
-import json
 
-ec2 = boto3.client('ec2')
-def lambda_handler(event, context):
-    response = ec2.describe_availability_zones()
-    return {"statusCode": 200, "body": json.dumps(response)}
+def lambda_handler(context, event):
+    dynamodb = boto3.resource('dynamodb', region_name='sa-east-1')
+    
+    table = dynamodb.create_table(
+        TableName='bruno_test_table_phonebook',
+        KeySchema=[
+            {
+                'AttributeName': 'ID',
+                'KeyType': 'HASH'  #Partition key
+            },
+            {
+                'AttributeName': 'Name',
+                'KeyType': 'RANGE'  #Sort key
+            }
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'ID',
+                'AttributeType': 'N'
+            },
+            {
+                'AttributeName': 'Name',
+                'AttributeType': 'S'
+            },
+    
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 10,
+            'WriteCapacityUnits': 10
+        }
+    )
+    
+    return ("Table status:", table.table_status)  
